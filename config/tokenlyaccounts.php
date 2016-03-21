669314c935f0d13375748a6ae2ec0e5e7f59669e
Devon Weller <devon@tokenly.co>
2016-03-21 06:32:28 -0500

    renamed config constant

--- config/tokenlyaccounts.php
+++ config/tokenlyaccounts.php
@@ -6,7 +6,7 @@ return [
     'client_secret' => env('TOKENLY_ACCOUNTS_CLIENT_SECRET',       'YOUR_TOKENLY_ACCOUNTS_CLIENT_SECRET_HERE'),
 
     // this is the URL that Tokenly Accounts uses to redirect the user back to your application
-    'redirect'      => env('TOKENLY_ACCOUNTS_PUBLIC_CALLBACK_URL', 'https://YourSiteHere.com/account/authorize/callback'),
+    'redirect'      => env('TOKENLY_ACCOUNTS_REDIRECT_URI', 'https://YourSiteHere.com/account/authorize/callback'),
 
     // this is the Tokenly Accounts URL
     'base_url'      => rtrim(env('TOKENLY_ACCOUNTS_PROVIDER_HOST', 'https://accounts.tokenly.com'), '/'),
